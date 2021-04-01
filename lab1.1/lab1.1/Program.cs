f1451c17ead06052fa5636ac802b347115972f8b
ykrop22 <45872447+ykrop22@users.noreply.github.com>
2021-04-01 17:02:52 +0300

    Add files via upload

---
+++ lab1.1/lab1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        delegate int avarage(int b, int c, int d);
+        static void Main(string[] args)
+        {
+            avarage Avarage = delegate (int b, int c, int d)
+            {
+                return (b + c + d) / 3;
+            };
+
+            int g = Avarage(5, 71, 19);
+            Console.WriteLine(g);
+            Console.Read();
+        }
+    }
+}
---
+++ lab1.2/lab1.2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        delegate float NumHandler(float num1, float num2);
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Выберите операцию:1-Сумма; 2-Разница; 3-Умножение; 4-Деление");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите первое число");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            float y = float.Parse(Console.ReadLine());
+
+
+            int caseSwitch = i;
+            switch (caseSwitch)
+            {
+                case 1:
+                    NumHandler Add = (x, y) => x + y;
+                    Console.WriteLine("Сумма чисел:");
+                    Console.WriteLine(Add(x, y));
+                    break;
+                case 2:
+                    NumHandler Sub = (x, y) => x - y;
+                    Console.WriteLine("Разница чисел:");
+                    Console.WriteLine(Sub(x, y));
+                    break;
+                case 3:
+                    NumHandler Mul = (x, y) => x * y;
+                    Console.WriteLine("Результат умножения чисел:");
+                    Console.WriteLine(Mul(x, y));
+                    break;
+                case 4:
+                    if (y == 0)
+                    {
+                        Console.WriteLine("Ошибка!!! На ноль делить нельзя!!!");
+                        break;
+                    }
+                    NumHandler Div = (x, y) => x / y;
+                    Console.WriteLine("Результат деления чисел:");
+                    Console.WriteLine(Div(x, y));
+                    break;
+
+
+            }
+        }
+    }
+}
